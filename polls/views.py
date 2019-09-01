00cbf3bd6bc219fc07e824d400f71452935d59c8
mars-taxi <weber.c.pan@gmail.com>
2019-09-01 20:57:44 +0800

    Tutorial 3 before writing actual views

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Question, Choice
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("This is a poll index. Try it out!!!")
+
+
+def detail(request, question_id):
+    response = "You are looking at the question %s:\n %s\n %s"
+    q = Question.objects.get(id=question_id)
+    question_text = q.question_text
+    question_pub_date = q.pub_date
+    return HttpResponse(response % (question_id, question_text, question_pub_date))
+
+
+def results(request, question_id):
+    response = "The result of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    response = "You're voting on question %s"
+    return HttpResponse(response % question_id)
